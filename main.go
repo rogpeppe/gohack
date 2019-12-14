a0664c0204fc437ed3ee5cb48c5129f60fdd21d6
Josh Bleecher Snyder <josharian@gmail.com>
2019-12-13 17:27:57 -0800

    delete large todo about subcommands
    Subcommands have been introduced.
    They don't perfectly match this structure,
    but it is reasonably close.
    The TODO comment contains a few as-yet unimplemented
    ideas for other features and subcommands:
    
    * get -u already has a todo in the get flags
    * rm isn't implemented, but it is also not likely to be forgotten if/when it is needed
    * dir has lots of overlap with #61, and the issue tracker is a better place to discuss
    
    Given that, the outdated comment no longer pulls its weight. Cull it.

--- main.go
+++ main.go
@@ -16,32 +16,6 @@ import (
 	"gopkg.in/errgo.v2/fmt/errors"
 )
 
-/*
-As the amount of functionality grows, it seems like we should consider having subcommands.
-
-A possible set of commands:
-
- gohack get [-vcs] [-f] [module...]
-Get gets the modules at the current version and adds replace statements to the go.mod file if they're not already replaced.
-If the -u flag is provided, the source code will also be updated to the current version if it's clean.
-If the -f flag is provided with -u, the source code will be updated even if it's not clean.
-If the -vcs flag is provided, it also checks out VCS information for the modules. If the modules were already gohacked in non-VCS mode, gohack switches them to VCS mode, preserving any changes made (this might result in the directory moving).
-
-With no module arguments and the -u flag, it will try to update all currently gohacked modules.
-
-gohack status
-Status prints a list of the replaced modules
-
-gohack rm [-f] module...
-Rm removes the gohack directory if it is clean and then runs gohack undo. If the -f flag is provided, the directory is removed even if it's not clean.
-
- gohack undo [module...]
-Undo removes the replace statements for the modules. If no modules are provided, it will undo all gohack replace statements. The gohack module directories are unaffected.
-
-gohack dir [-vcs] [module...]
-Dir prints the gohack module directory names for the given modules. If no modules are given, all the currently gohacked module directories are printed. If the -vcs flag is provided, the directory to be used in VCS mode is printed. Unlike the other subcommands, the modules don't need to be referenced by the current module.
-*/
-
 var (
 	printCommands = flag.Bool("x", false, "show executed commands")
 	dryRun        = flag.Bool("n", false, "print but do not execute update commands")
